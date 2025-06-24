ab44c1e329ad6928596dfd015c00e22993b6233c
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-06-24 15:06:14 -0400

    add tag container note

---
+++ src/core/platform/container/tag.rs
@@ -0,0 +1,5 @@
+/*
+Tag Container
+
+Tags are used for organizing and categorizing resources within the application. These are lightweight, flexible constructs that can be attached to various entities, such as content items, users, and workflows. Tags enable efficient filtering, searching, and management of resources, enhancing the overall usability and organization of the system.
+*/

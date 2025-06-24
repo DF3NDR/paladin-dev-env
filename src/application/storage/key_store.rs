1e66f652e36e40693429993b80faf426267d8d1e
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-06-24 15:04:42 -0400

    descriptions in key_ key_value_store

--- src/application/storage/key_store.rs
+++ src/application/storage/key_store.rs
@@ -2,18 +2,18 @@
 
 Key Store Port
 
-A port that defines how the application stores and retrieves keys. This would typically be 
-a secure system for storing and retrieving keys, such as a key management service, a hardware
+A port that defines how the application stores and validates keys. This is a
+a secure system such as a key management service, a hardware
 security module, or any other secure key storage mechanism.
 
 The key store port is responsible for providing an abstraction layer that allows the application
-to securely store and retrieve keys without being tightly coupled to the details of how those keys
+to securely store and validate keys without being tightly coupled to the details of how those keys
 are stored and retrieved. This way a centralized storage mechanism can be used to manage keys
 across the application and other applications.  
 
 A typical implementation of this port would be for the adapter to translate key storage 
-and retrieval requests from the application into calls to the key storage mechanism, and to
-translate key storage and retrieval responses from the key storage mechanism back into a format
+and validation requests from the application into calls to the key storage mechanism, and to
+translate responses from the key storage mechanism back into a format
 that the application can use.
 
 Configuration of the environment and access control to the key store would be managed by the
--- src/application/storage/key_value_store.rs
+++ src/application/storage/key_value_store.rs
@@ -1,14 +1,13 @@
 /*
 
-Key Value Store
+Key Value Store Port
 
-A key value store is a simple storage mechanism that stores keys and values in a simple key value
-pair. This is a simple storage mechanism that can be used to store and retrieve data in a simple
-and efficient manner.
+A key value store is a port for the storage mechanisms that store simple key value
+pairs.
 
-This key value store is a simple in-memory store that stores keys and values in a HashMap. This
-store is not persistent and will lose all data when the application is restarted.
+This key value store is an in-memory store that typically stores values in a HashMap. This
+store is not persistent and will lose all data when the application is restarted unless otherwise specified.
 
 This store is used for use cases where a temporary storage mechanism is needed, such as caching
-'values' that need to be repeatedly accessed.
+'values' that need to be repeatedly accessed or queueing items.
 */
